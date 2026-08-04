7cf806c25c686da43345721e55229e59e8cfde9e
Ravi <ravikant@aptaracorp.com>
2026-08-04 19:53:50 +0530

    Add project files.

---
+++ Controllers/TesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPIHTTPEndpointsWithoutPostman.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello, world!";
+        }
+
+
+        [HttpPost]
+        public string Post([FromBody] string name)
+        {
+            return "Hello, " + name + "!";
+        }
+
+    }
+}
